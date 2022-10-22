d7dd3504f78c8761d056270ef31140b0c072a033
Студент1 <1@DESKTOP-7RO4MU5>
2022-10-22 12:19:37 +0300

    Добавьте файлы проекта.

---
+++ Fib/Program.cs
@@ -0,0 +1,32 @@
+﻿
+global::System.
+    Console.WriteLine("Введите число из числового порядка Фибаначчи чтобы узнать его порядок");
+string a = Console.ReadLine();
+int b = int.Parse(a);
+int first = 0;
+int second = 1;
+int result = 0;
+int count = 0;
+void fub(int b)
+{
+    for (int i = 0;; i++)
+    {
+
+        result = first + second;
+        first = second;
+        second =result;
+        count++;
+
+
+        if (result == b)
+        {
+            Console.WriteLine($"Число в последовательности - {count+2}");
+            break;
+        }
+      
+
+
+    }
+    
+}
+fub(b);
